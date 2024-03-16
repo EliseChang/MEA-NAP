0adfe008ee03615355d77b7f0ffe5378f75e8b84
Elise Chang <elisecwchang@gmail.com>
2024-03-16 15:24:06 +0000

    Added output directory to batch conversion script

--- Functions/convertRawToMat/MEA_load_bin.m
+++ Functions/convertRawToMat/MEA_load_bin.m
@@ -1,27 +1,31 @@
-function [header,m,channels] = MEA_load_bin(binfile,plt, convertOption)
-% Convert bin files to mat files for MEA data
-% This function takes txt file output from MC_Datatool (Multichannel
-% Systems) and converts it into a matlab matrix.
-% Parameters 
-% -----------
-% binfile : str 
-%      path to the bin file to be converted
-% plt : bool
-%      whether to plot
-% convertOption: str
-%      'whole' - converts the bin file into one files with one matrix 
-%      'electrode' - convert the bin file into files each with one electrde
-%     
-% Notes
-% -------------
+function [header,m,channels] = MEA_load_bin(binfile,plt, convertOption, outputDir)
+% note that I (TS) is going to modify this slightly to make it work with
+% any number of channels 
+% also changed the code so that it saves one giant variable 
+% rather than create a subfolder 
+% also makes running combineMat.m unncessary 
+
+%form: 
+%This function takes txt file output from MC_Datatool (Multichannel
+%Systems) and converts it into a matlab matrix.
+%
+
+% Instructions 
+
 % Do not run this directly, this is called by MEAbatchConvert.m 
 % But a lot of the key steps of the conversion processes are contained here
-% 
+% The main thing to change is 'convertOption', which you should specify in
+% MEAbatchConvert.m, the default is to do 'whole'; the entire grid will be
+% saved in one variable 
+% the other option is to do electrode by electrode conversion, in which
+% case convertOption == 'electrode'
+
+% Last update: 20180626 
+
 % Log
-% ---------------
-% 2018-06-26: Tim Sit - Added option to save each electrode as an individual .mat file 
+% 20180626: Added option to save each electrode as an individual .mat file 
 % (to avoid memory issues on systems with less than 16GB ram / low swap space)
-% 2023-11-13 : Tim Sit - Updated documentation
+
 
 %% initialize
 
@@ -35,7 +39,10 @@ if ~exist('convertOption', 'var')
     convertOption = 'whole'; 
 end 
 
-
+dir = pwd;
+if exist('outputDir','var')
+    addpath(outputDir)
+end
 
 %% get header
 [fid] =fopen(binfile)
@@ -45,8 +52,8 @@ fclose(fid);
 
 %% get key values from header
 
-m = m';
-f = findstr(m,'EOH');
+m=m';
+f=findstr(m,'EOH');
 newstart=f+4;
 header=m(1:f-1)
 
@@ -62,16 +69,16 @@ uV = str2num(m(uVf-7:uVf-2));
 
 %% get channels from header
 
-chf = findstr(header,'Streams');
-chs = header(chf+11:length(header));
+chf=findstr(header,'Streams');
+chs = header(chf+11:length(header))
 channels=[];
-
 while length(chs)>5
-    f = findstr(chs,'_');
-    channels = [channels; str2num(chs(f(1)+1:f(1)+2))];
+    f=findstr(chs,'_');
+    channels=[channels; str2num(chs(f(1)+1:f(1)+2))];
     chs(1:f(1)+2)=[];
-end
-
+end;
+channels;
+size(channels)
 %% get data
 
 [fid] =fopen(binfile);
@@ -84,6 +91,10 @@ fclose(fid);
 %each row is a different channel
 numChannels = length(channels); 
 m=reshape(data,numChannels,floor(length(data)/numChannels));
+% note that I (TS) changed '60' to numChannels 
+% not sure if there will be any consequences... 
+% need to check through the logic of this code to make sure
+
 
 %% convert data to microvolts (see MC_datatool help - search 'binary')
 max(m(:));
@@ -95,25 +106,29 @@ dat = m'; % matlab reads tall matrices more efficiently
 % tall means number of rows > number of columns
 %% plot
 
+
 if plt
     ch=82;
     figure
     t=1:size(m,2);
     t=t/fs;
     size(m)
-    f = find(channels==ch);
+    f=find(channels==ch)
     plot(t,m(f(1),:),'k')
-end
+end;
 
 %% save
 sprintf('Saving data ...')
 
-% mkdir(binfile(1:length(binfile)-4))
+% /mkdir(binfile(1:length(binfile)-4))
 % the crux to not having folders is here 
 % save([binfile(1:length(binfile)-4) filesep binfile(1:length(binfile)-4) '.mat'],'dat','channels','header','uV','ADCz','fs')
 % the above line saves to specific subfolders 
 
 % OPTION 1: Save all electrodes as a single file (about 2GB variable), only a good idea if your system has 16GB ram / a lot of swap space
+if exist('outputDir','var')
+    cd(outputDir)
+end
 if strcmp(convertOption, 'whole')
     save([binfile(1:length(binfile)-4) '.mat'],'dat','channels','header','uV','ADCz','fs', '-v7.3')
 % OPTION 2: For systems with 8GB ram (or less?), save each electrode in a separate file 
@@ -123,9 +138,9 @@ elseif strcmp(convertOption, 'electrode')
          sprintf('Channel: %d; ',channels(i))
          dat=m(i,:);
          save([binfile(1:length(binfile)-4) filesep binfile(1:length(binfile)-4) '_' num2str(channels(i)) '.mat'],'dat','channels','header','uV','ADCz','fs')
-    end 
-    
-end 
+    end    
+end
+cd(dir) % this will always be the initial directory
 
 end
 
